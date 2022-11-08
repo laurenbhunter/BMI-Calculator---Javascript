26bba35d5b4b270650c3da2904abd6106c3b8c5e
Lauren Hunter <107400486+laurenbhunter@users.noreply.github.com>
2022-11-08 18:19:28 +0100

    Add files via upload

---
+++ bmi_calculator_javascript/script.js
@@ -0,0 +1,29 @@
+//function
+//get values
+//calculate
+//output
+
+//bmi formula weight/height squared * 703
+
+function calculateBmi(){
+  let weight = document.getElementById('weight').value
+  let height = document.getElementById('height').value
+
+  let bmi = (weight / (height * height) * 703)
+
+  document.getElementById('heading').innerHTML = 'Your BMI is ...'
+  document.getElementById('bmi-output').innerHTML = bmi.toFixed(2)
+
+  if (bmi <= 24.9) {
+    document.getElementById('message').innerHTML = 'You are underweight'
+  } else if (bmi => 25 && bmi <= 29.9) {
+    docuement.getElementById('message').innerHTML = 'You are a healthy weight'
+  } else {
+  docuement.getElementById('message').innerHTML = 'You are overweight'
+  }
+
+}
+function reload () {
+
+  window.location.reload()
+}
